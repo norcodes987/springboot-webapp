cbd54a369e74f3478472296360af360dc343bc0d
eleanortay <eleanortay@localhost>
2023-10-27 15:11:10 +0800

    implement delete, update and add todo

--- myfirstwebapp/src/main/java/com/eleanor/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/src/main/java/com/eleanor/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,11 +1,17 @@
 package com.eleanor.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import jakarta.validation.Valid;
 
 @Controller
 @SessionAttributes("name")
@@ -24,6 +30,53 @@ public class TodoController {
 		model.addAttribute("todos", todos);
 		
 		return "listTodos";
+	}
+	
+	@RequestMapping(value="add-todo", method=RequestMethod.GET)
+	public String showNewTodos(ModelMap model) {
+		String username = (String)model.get("name");
+		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+		model.put("todo", todo);
+		return "todo";
+	}
+	
+	//add item
+	@RequestMapping(value="add-todo", method=RequestMethod.POST)
+	public String addNewTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			return "todo";
+		}
+		String username = (String)model.get("name");
+		todoService.addTodo(username, todo.getDescription(), todo.getTargetDate(), false);
+		return "redirect:list-todos";
+	}
+	
+	//delete item
+	@RequestMapping("delete-todo")
+	public String deletelTodo(@RequestParam int id) {
+		todoService.deleteById(id);
+		return "redirect:list-todos";
+	}
+	
+	//show update todo page
+	@RequestMapping(value="update-todo", method=RequestMethod.GET)
+	public String showUpdateTodoPage(@RequestParam int id, ModelMap model) {
+		Todo todo = todoService.findbyId(id);
+		model.addAttribute("todo", todo);
+		return "todo";
+	}
+	
+	//update todo
+	@RequestMapping(value="update-todo", method=RequestMethod.POST)
+	public String updateTodo(ModelMap model, @Valid Todo todo, BindingResult result) {
+		if (result.hasErrors()) {
+			return "todo";
+		}
+		String username = (String)model.get("name");
+		todo.setUsername(username);
+		todoService.updateTodo(todo);
+		return "redirect:list-todos";
 	}
 
 }
--- myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+++ myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -1,13 +1,10 @@
-<html>
-	<head>
-		<title>Welcome Page</title>
-	</head>
-	<body>
+
+<%@ include file="common/header.jspf" %>
+<%@ include file="common/navigation.jspf" %>
 	<div class="container">
 		<h1>Welcome ${name}</h1>
 		<hr>
 		<div>Your name: ${name}</div>
 		<div><a href="list-todos">Manage</a> your todos</div>
 		</div>
-	</body>
-</html>
+<%@ include file="common/footer.jspf" %>
